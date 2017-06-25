8123788bf27a47d1972e3f02a2c351bb612560f0
melunar <2515768597@qq.com>
2017-06-25 20:59:59 +0800

    【编辑】首页分类展示

--- routers/main.js
+++ routers/main.js
@@ -4,11 +4,20 @@
 
 var express = require("express");
 var router = express.Router();
+var Category = require("../models/Category.js");
 
 //监听 /
 router.get("/", function(req, res, next) {
-	var data = { userInfo: req.userInfo };
-	res.render("index", data); //req.userInfo app.js定义了
+	//获取分类
+	Category.find().sort({_id: -1}).then(function(cats) {
+		//console.log(cats);
+		res.render("index",  {
+			userInfo: req.userInfo,
+			categories: cats
+		}); //req.userInfo app.js定义了
+	});
+
+
 });
 
 module.exports = router;
